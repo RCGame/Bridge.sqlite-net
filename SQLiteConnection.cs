0956514fdeb01b8e0b85afc0f5af615478dfe885
Ramon Chan <rc.game.studio@gmail.com>
2018-08-31 23:42:11 +1000

    some updates

--- SQLiteConnection.cs
+++ SQLiteConnection.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using Bridge.Html5;
 
 namespace SQLite
 {
@@ -18,47 +19,99 @@ namespace SQLite
     //Html5 Local Storage implementation of SQLite
     public class SQLiteConnection
     {
+        private string prefix;
 
         public SQLiteConnection(string path)
         {
-
+            prefix = path + ".";
         }
 
         public void CreateTable<T>() where T : new()
         {
-            T b = new T();
-            string json = JsonConvert.SerializeObject(b);
-            var linq = typeof(T).GetProperties().Where(t => t.GetCustomAttributes().Count() > 0);
-            if (linq.Count() > 0)
+            T row = new T();
+            List<T> rows = new List<T>();
+            rows.Add(row);
+            string json = JsonConvert.SerializeObject(rows);
+            var item = Window.LocalStorage.GetItem(prefix + typeof(T).Name);
+            if (item == null)
             {
-                var attrs = linq.First().GetCustomAttributes();
-                Console.WriteLine(linq.First().Name + " " + attrs.Aggregate((c, d) => c.ToString() + "," + d.ToString()));
+                Window.LocalStorage.SetItem(prefix + typeof(T).Name, json);
             }
-            Console.WriteLine(json);
         }
 
         public T Insert<T>(T row)
         {
-            throw new NotImplementedException();
+            var item = Window.LocalStorage.GetItem(prefix + typeof(T).Name);
+            if (item != null)
+            {
+                List<T> rows = JsonConvert.DeserializeObject<List<T>>(item.ToString());
+                var linq = typeof(T).GetProperties().Where(t => t.GetCustomAttributes().Count() > 0);
+                foreach (var property in linq)
+                {
+                    var name = property.Name;
+                    var attrs = property.GetCustomAttributes();
+                    if (attrs.Where(t => t.ToString() == "SQLite.AutoIncrement").Count() > 0)
+                    {
+                        row[name] = rows.Max(t => (int)t[name]) + 1;
+                        rows.Add(row);
+                        Window.LocalStorage.SetItem(prefix + typeof(T).Name, JsonConvert.SerializeObject(rows));
+                    }
+                }              
+            }
+            return row;
         }
 
         public void Update<T>(T row)
         {
-
+            var item = Window.LocalStorage.GetItem(prefix + typeof(T).Name);
+            if (item != null)
+            {
+                List<T> rows = JsonConvert.DeserializeObject<List<T>>(item.ToString());
+                var linq = typeof(T).GetProperties().Where(t => t.GetCustomAttributes().Count() > 0);
+                foreach (var property in linq)
+                {
+                    var name = property.Name;
+                    var attrs = property.GetCustomAttributes();
+                    if (attrs.Where(t => t.ToString() == "SQLite.PrimaryKey").Count() > 0)
+                    {
+                        var target = rows.Where(t => t[name] == row[name]).First();
+                        foreach (var prop in typeof(T).GetProperties())
+                        {
+                            target[prop.Name] = row[prop.Name];
+                        }
+                        Window.LocalStorage.SetItem(prefix + typeof(T).Name, JsonConvert.SerializeObject(rows));
+                    }
+                }
+            }
         }
 
         public void UpdateAll<T>(IEnumerable<T> rows)
         {
-
+            foreach (var row in rows)
+            {
+                Update(row);
+            }
         }
 
         public void InsertAll<T>(IEnumerable<T> rows)
         {
-
+            foreach (var row in rows)
+            {
+                Insert(row);
+            }
         }
 
         public int DeleteAll<T>()
         {
+            string json = "[]";
+            var item = Window.LocalStorage.GetItem(prefix + typeof(T).Name);
+            if (item == null)
+            {
+                var tmp = Window.LocalStorage.GetItem(prefix + typeof(T).Name).ToString();
+                var rows = JsonConvert.DeserializeObject<List<T>>(tmp);
+                Window.LocalStorage.SetItem(prefix + typeof(T).Name, json);
+                return rows.Count();
+            }
             return 0;
         }
 
